f5ccb9477a0c5d922e654f7f9403d21534c3616b
Bo-cheng Jan <school510587@yahoo.com.tw>
2014-01-08 03:26:48 +0800

    Read GuitarBot configuration
    Changes:
    1. Let main.php require two runtime arguments.
    2. Define and implement <config_script>. Please see README for detailed
       description.

--- README.md
+++ README.md
@@ -16,4 +16,28 @@ the command-line interface of PHP5. It can become available using the command:
 
 , and the command runs this project:
 
-     $ php main.php
+     $ php -e main.php <config_script> <score_script>
+
+The two scripts will be described in detail. You can choose GuitarBot.ini as
+<config_script> input.
+
+[-] Hardware configuration <config_script>
+     This script defines four latency times: move, press, fret and release. One
+line is one single specification of a latency time. Their formats:
+
+     move_latency <from_string> <to_string> <value>
+     press_latency <value>
+     fret_latency <value>
+     release_latency <value>
+
+     move_latency is the delay for motor movement from a position (say A) to
+another position (say B). A->B and B->A can be assigned with different values,
+if only one of them is assigned then they both share the same value.
+     press_latency is the delay for motor movement from [release] to [press]
+state.
+     fret_latency is the minimum time from [fret] to [release] command that
+human can hear the pitch played on a guitar.
+     release_latency is the delay for motor movement from [press] to [release]
+state.
+     All <value> are measured in seconds, while <from_string> and <to_string>
+must be positive integers.
--- main.php
+++ main.php
@@ -1,4 +1,6 @@
 <?php
+ count($argv) == 3 or die("Usage: php -e $argv[0] <config_script> <score_script>\n");
+ $latency = read_config($argv[1]);
  $sheet = "^(3510)16 | (45)4 (34)4 (23)4 (1423)4 | ......";
  preg_match_all("/\^?\(((\d\d)+)\)(\d+)/", $sheet, $score, PREG_SET_ORDER);
  $string_map = array();
@@ -17,4 +19,64 @@
   $time_axis += $tuple[3];
  }
  print_r($string_map);
+
+function read_config($source)
+{
+ $latency = array();
+ $config = file_get_contents($source) or die("Error reading $source\n");
+ $config = preg_split("/\n|\r/", $config, -1, PREG_SPLIT_NO_EMPTY);
+ foreach ($config as $line) {
+  $data = preg_split("/\s/", $line, -1, PREG_SPLIT_NO_EMPTY);
+  switch ($data[0]) {
+   case "fret_latency":
+    count($data) == 2 or die("Error fret_latency assignment\n");
+    !array_key_exists("fret", $latency) or die("Conflict assignment of fret_latency\n");
+    $latency["fret"] = to_nonnegative_float($data[1], "Error format of fret_latency\n");
+   break;
+   case "move_latency":
+    count($data) == 4 or die("Error move_latency assignment\n");
+    if (!array_key_exists("move", $latency))
+     $latency["move"] = array();
+    $data[1] = to_positive_int($data[1], "Error format of move_latency parameter #1\n");
+    $data[2] = to_positive_int($data[2], "Error format of move_latency parameter #2\n");
+    if (!array_key_exists($data[1], $latency["move"]))
+     $latency["move"][$data[1]] = array();
+    !array_key_exists($data[2], $latency["move"][$data[1]]) or die("Conflict assignment of move_latency $data[1] $data[2]\n");
+    $data[1] != $data[2] or die("move_latency cannot apply to the same position\n");
+    $latency["move"][$data[1]][$data[2]] = to_nonnegative_float($data[3], "Error format of move_latency parameter #3\n");
+   break;
+   case "press_latency":
+    count($data) == 2 or die("Error press_latency assignment\n");
+    !array_key_exists("press", $latency) or die("Conflict assignment of press_latency\n");
+    $latency["press"] = to_nonnegative_float($data[1], "Error format of press_latency\n");
+   break;
+   case "release_latency":
+    count($data) == 2 or die("Error release_latency assignment\n");
+    !array_key_exists("release", $latency) or die("Conflict assignment of release_latency\n");
+    $latency["release"] = to_nonnegative_float($data[1], "Error format of release_latency\n");
+   break;
+  }
+ }
+ foreach ($latency["move"] as $s => $path) {
+  foreach (array_keys($path) as $t) {
+   if (!array_key_exists($t, $latency["move"]))
+    $latency["move"][$t] = array();
+   if (!array_key_exists($s, $latency["move"][$t]))
+    $latency["move"][$t][$s] = $latency["move"][$s][$t];
+  }
+ }
+ return $latency;
+}
+
+function to_nonnegative_float($f, $e)
+{
+ preg_match("/^\s*\d+(\.\d+)?\s*$/", $f) or die($e);
+ return (float)$f;
+}
+
+function to_positive_int($n, $e)
+{
+ preg_match("/^\s*[1-9]\d*\s*$/", $n) or die($e);
+ return (int)$n;
+}
 ?>
